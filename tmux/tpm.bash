b085084db13be1027f4b58e090e124725b877ae9
Nathan Genetzky <Nathan@Genetzky.us>
2017-12-25 23:24:25 -0600

    tmux: Tmux Plugin Manager is installed via tpm.bash

---
+++ tmux/tpm.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# global
+
+BASEDIR="$(readlink -f $(dirname $BASH_SOURCE))"
+
+# functions
+
+do_fetch() {
+    [[ -d ~/.tmux/plugins/tpm ]] && return 0
+    git clone --depth=1 https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+}
+
+do_install() {
+    [[ -d ~/.tmux/plugins/tpm ]] || return 1
+    ~/.tmux/plugins/tpm/bin/install_plugins
+}
+
+do_clean() {
+    [[ -d ~/.tmux/plugins/tpm ]] || return 0
+    rm -r \
+        ~/.tmux/plugins/* \
+        ~/.tmux/sidebar/*
+}
+
+# main
+
+main() {
+    local action="${1-default}"
+
+    case $action in
+        fetch)
+            do_fetch
+            ;;
+        install)
+            do_install
+            ;;
+        clean)
+            do_clean
+            ;;
+        default)
+            do_fetch
+            do_install
+    esac
+
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    # Strict Mode
+    set -euo pipefail
+    IFS=$'\n\t'
+
+    main "$@"
+fi
