15eb71ce1befa7804e93bc40236d205878b9d81c
Nathan Genetzky <nathan@genetzky.us>
2018-12-28 23:48:25 -0600

    downloads: gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2

---
+++ downloads/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPTDIR="$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)"
+source "${SCRIPTDIR}/_downloads.bash"
+
+# Bash Strict Mode
+set -eu -o pipefail
+
+wget_download  \
+	'https://launchpad.net/gcc-arm-embedded/5.0/5-2016-q1-update/+download/gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2' \
+	'gcc-arm-none-eabi-5_3-2016q1-20160330-linux.tar.bz2'
