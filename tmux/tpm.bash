6618f645b2c4f74cba62e703f77055cb301cbcb0
Nathan Genetzky <nathan@genetzky.us>
2019-06-15 03:57:00 -0500

    tpm: Fix plugin

--- tmux/tpm.bash
+++ tmux/tpm.bash
@@ -6,23 +6,26 @@ GITROOT="$(git rev-parse --show-toplevel)"
 BASEDIR="${GITROOT}/tmux/"
 # BASEDIR="$(readlink -f $(dirname $BASH_SOURCE))"
 
+SRCDIR="${HOME}/.tmux/plugins/tpm"
+
 # functions
 
 do_fetch() {
-    [[ -d ~/.tmux/plugins/tpm ]] && return 0
-    git clone --depth=1 https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+    [[ ! -d "${SRCDIR}" ]] || return 0
+    git clone --depth=1 https://github.com/tmux-plugins/tpm.git "${SRCDIR}"
 }
 
 do_install() {
-    [[ -d ~/.tmux/plugins/tpm ]] || return 1
-    ~/.tmux/plugins/tpm/bin/install_plugins
+    [[ -d "${SRCDIR}" ]] || return 1
+     "${SRCDIR}/bin/install_plugins" || true
+    install -d "${HOME}/.tmux/themes/"
     ln -fTs \
-        "${BASEDIR}/themes" \
-        "${HOME}/.tmux/themes"
+        "${GITROOT}/config/tmux/themes/lateo.conf" \
+        "${HOME}/.tmux/themes/lateo.conf"
 }
 
 do_clean() {
-    [[ -d ~/.tmux/plugins/tpm ]] || return 0
+    [[ -d "${SRCDIR}" ]] || return 0
     rm -r \
         ~/.tmux/plugins/ \
         ~/.tmux/sidebar/ \
