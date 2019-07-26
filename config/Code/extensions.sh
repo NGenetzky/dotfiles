27ea0673923321a47bd8292d1bc322bdf15e6e26
Nathan Genetzky <nathan@genetzky.us>
2019-07-25 21:31:21 -0500

    config/Code: Disable extensions

--- config/Code/extensions.sh
+++ config/Code/extensions.sh
@@ -3,8 +3,6 @@
 code --install-extension Braver.vscode-solarized
 code --install-extension CoenraadS.bracket-pair-colorizer
 code --install-extension EugenWiens.bitbake
-code --install-extension GitHub.vscode-pull-request-github
-code --install-extension KnisterPeter.vscode-github
 code --install-extension PKief.material-icon-theme
 code --install-extension PeterJausovec.vscode-docker
 code --install-extension YoshinoriN.current-file-path
@@ -17,9 +15,6 @@ code --install-extension codezombiech.gitignore
 code --install-extension eamodio.gitlens
 code --install-extension emilast.LogFileHighlighter
 code --install-extension fabiospampinato.vscode-terminals
-code --install-extension fatihacet.gitlab-workflow
-code --install-extension gioboa.jira-plugin
-code --install-extension jduponchelle.jira-link
 code --install-extension keroc.hex-fmt
 code --install-extension letmaik.git-tree-compare
 code --install-extension maelvalais.autoconf
@@ -28,12 +23,19 @@ code --install-extension matthias-schwarze.vimter
 code --install-extension ms-python.python
 code --install-extension ms-vscode.cpptools
 code --install-extension plorefice.devicetree
-code --install-extension pstreule.codebucket
 code --install-extension qezhu.gitlink
 code --install-extension ryuta46.multi-command
 code --install-extension vscodevim.vim
 code --install-extension yzhang.markdown-all-in-one
 code --install-extension ziyasal.vscode-open-in-github
+
+# Disabled, install on workspace-by-workspace basis.
+# code --install-extension GitHub.vscode-pull-request-github
+# code --install-extension KnisterPeter.vscode-github
+# code --install-extension fatihacet.gitlab-workflow
+# code --install-extension gioboa.jira-plugin
+# code --install-extension jduponchelle.jira-link
+# code --install-extension pstreule.codebucket
 
 # These are unofficial
 # code --install-extension particle.particle-vscode-core
