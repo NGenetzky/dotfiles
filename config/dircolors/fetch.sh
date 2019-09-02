b8d4d98abc01f04bc748cbe7a3a4fce748b659d9
Nathan Genetzky <nathan@genetzky.us>
2019-09-02 02:30:54 -0500

    config/dircolors: Download solarized dircolors

---
+++ config/dircolors/fetch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget 'https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark' \
+    -O 'dircolors.256dark'
+wget 'https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.ansi-dark' \
+    -O 'dircolors.ansi-dark'
